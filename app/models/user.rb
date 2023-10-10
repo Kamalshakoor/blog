5b7e3b202196e35c5eb4042473c2b314cbeaeedd
kamal shakoor <kamalshakoor29022000.com>
2023-10-10 19:19:58 +0500

    Add users validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
