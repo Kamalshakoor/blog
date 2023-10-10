9d8d206294ea8d03b22bd02dd68b546623ee009d
kamal shakoor <kamalshakoor29022000.com>
2023-10-10 23:50:33 +0500

    user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
     has_many :articles
     validates :username, presence: true, uniqueness: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
