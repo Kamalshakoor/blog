5ab6e61998d6ac3313cf17390764351e614d5bae
kamal shakoor <kamalshakoor29022000.com>
2023-10-10 19:56:44 +0500

    Added one-to-many relation between users and articles table on backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :username, presence: true, uniqueness: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
