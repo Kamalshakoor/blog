f714f6a5f97a9bc592bd285e0709e7f1bd48c69c
kamal shakoor <kamalshakoor29022000.com>
2023-10-10 18:29:00 +0500

    First commit = Article crud completed along with its designing

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show,:edit,:update,:destroy]
+
+    def home
+    end
+
+    def show
+        
+    end
+
+    def index
+        @articles= Article.all.order("created_at DESC")
+    end
+
+    def new
+        @article=Article.new
+    end
+
+    def edit
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        if @article.update(article_params)
+            redirect_to '/articles'
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @article.destroy
+        redirect_to '/articles'
+    end
+
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+end
