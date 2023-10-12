423e61af2c5a8426ce045ded1665389205c459f5
kamal shakoor <kamalshakoor29022000.com>
2023-10-12 19:29:34 +0500

    Added delete account and all associated article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save {self.email = email.downcase}
-    has_many :articles
+    has_many :articles, dependent: :destroy
     validates :username, presence: true, uniqueness: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
